b082d32e03848e90efa9c2ac6dd480e1b49ccc75
gabeFrancisco <gabegit2@gmail.com>
2025-12-28 10:19:16 -0300

    criado estrutura inicial do Spring

---
+++ sistemanotas/src/main/java/dev/ellyon/sistemanotas/SistemanotasApplication.java
@@ -0,0 +1,13 @@
+package dev.ellyon.sistemanotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemanotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemanotasApplication.class, args);
+	}
+
+}
